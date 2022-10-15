55fd6eaf38fd469d0c75a7ea5a31c38a462bf881
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-16 00:03:00 +0200

    add files to source control

--- ARCoaching/ContentView.swift
+++ ARCoaching/ContentView.swift
@@ -5,39 +5,58 @@
 //  Created by Zaid Neurothrone on 2022-10-15.
 //
 
-import SwiftUI
+import ARKit
 import RealityKit
+import SwiftUI
 
 struct ContentView : View {
-    var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+  var body: some View {
+    ARViewContainer().edgesIgnoringSafeArea(.all)
+  }
+}
+
+extension ARView: ARCoachingOverlayViewDelegate {
+  func addCoaching() {
+    let coachingOverlay = ARCoachingOverlayView()
+    coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    coachingOverlay.goal = .horizontalPlane
+    coachingOverlay.session = self.session
+    coachingOverlay.delegate = self
+    self.addSubview(coachingOverlay)
+  }
+  
+  private func addVirtualObjects() {
+    let box = ModelEntity(mesh: .generateBox(size: 0.3), materials: [SimpleMaterial(color: .green, isMetallic: true)])
+    
+    guard let anchor = self.scene.anchors.first(where: { $0.name == "Plane Anchor" }) else {
+      return
     }
+    
+    anchor.addChild(box)
+  }
+  
+  public func coachingOverlayViewDidDeactivate(_ coachingOverlayView: ARCoachingOverlayView) {
+    addVirtualObjects()
+  }
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    
-    func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
-        return arView
-        
-    }
-    
-    func updateUIView(_ uiView: ARView, context: Context) {}
-    
+  func makeUIView(context: Context) -> ARView {
+    let arView = ARView(frame: .zero)
+    let anchor = AnchorEntity(plane: .horizontal)
+    anchor.name = "Plane Anchor"
+    arView.scene.addAnchor(anchor)
+    arView.addCoaching()
+    return arView
+  }
+  
+  func updateUIView(_ uiView: ARView, context: Context) {}
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
 #endif
